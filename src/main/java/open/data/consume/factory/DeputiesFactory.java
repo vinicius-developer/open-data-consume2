dab347649f2016522a54197258d88726927c12d0
vinicius-developer <viniciusilvav1@hotmail.com>
2022-08-30 20:54:15 +0000

    Deputies factory

--- src/main/java/open/data/consume/factory/DeputiesFactory.java
+++ src/main/java/open/data/consume/factory/DeputiesFactory.java
@@ -1,5 +1,19 @@
 package open.data.consume.factory;
 
+import java.io.IOException;
+
+import open.data.consume.deputados.lista.ListaDeputados;
+import open.data.consume.deputados.lista.ds.WrapperResultadoListaDeputados;
+import open.data.consume.http.HttpClientApi;
+
 public class DeputiesFactory {
+
+    public WrapperResultadoListaDeputados list(String parametros) 
+            throws IOException, InterruptedException {
+
+        return new ListaDeputados(new HttpClientApi(), parametros)
+            .pegar();
+
+    }
     
 }
