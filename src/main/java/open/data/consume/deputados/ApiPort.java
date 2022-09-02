e6fa3832a811340165bec698f913b66adbb0c828
Vinicius de Paula <viniciusilvav1@hotmail.com>
2022-09-02 10:49:57 -0300

    modify ApiPort

--- src/main/java/open/data/consume/deputados/ApiPort.java
+++ src/main/java/open/data/consume/deputados/ApiPort.java
@@ -2,10 +2,8 @@ package open.data.consume.deputados;
 
 import java.io.IOException;
 
-import open.data.consume.deputados.lista.ds.WrapperResultadoListaDeputados;
-
 public interface ApiPort<T> {
 
-    WrapperResultadoListaDeputados pegar() throws IOException, InterruptedException;
+    T pegar() throws IOException, InterruptedException;
 
 }
