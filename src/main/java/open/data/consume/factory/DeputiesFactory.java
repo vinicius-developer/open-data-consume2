a8f5fbc6eaf2c999a0e5e432b6ae591bd13f5cb8
vinicius-developer <viniciusilvav1@hotmail.com>
2022-09-05 19:34:03 +0000

    get deputie info

--- src/main/java/open/data/consume/factory/DeputiesFactory.java
+++ src/main/java/open/data/consume/factory/DeputiesFactory.java
@@ -2,6 +2,8 @@ package open.data.consume.factory;
 
 import java.io.IOException;
 
+import open.data.consume.deputados.info.InformacaoDeputados;
+import open.data.consume.deputados.info.ds.WrapperResultadoInformacaoDeputado;
 import open.data.consume.deputados.lista.ListaDeputados;
 import open.data.consume.deputados.lista.ds.WrapperResultadoListaDeputados;
 import open.data.consume.http.HttpClientApi;
@@ -14,6 +16,12 @@ public class DeputiesFactory {
         return new ListaDeputados(new HttpClientApi(), parametros)
             .pegar();
 
+    }
+
+    public WrapperResultadoInformacaoDeputado info(Integer id) 
+            throws IOException, InterruptedException {
+        return new InformacaoDeputados(new HttpClientApi(), id)
+            .pegar();
     }
     
 }
